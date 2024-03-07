4e4b5e017b741302e9a45197117e59f54dfbcc21
Kavindu <kavindu2308@gmail.com>
2024-03-08 02:12:43 +0530

    update for the customer post request

--- routes/customer.js
+++ routes/customer.js
@@ -1,20 +1,31 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Customer = require('../model/CustomerModel');
 
-router.get('/', (req,res) => {
-    res.send(`Customer API just Called`);
-})
-
-router.get('/:id', (req,res) =>{
-    res.send(`Customer with id ${req.params.id} just called!`);
+router.get("/", (req, res) => {
+  res.send(`Customer API just Called`);
 });
 
-router.post('/', (req,res) => {
-    res.send(req.body);
+router.get("/:id", (req, res) => {
+  res.send(`Customer with id ${req.params.id} just called!`);
 });
 
-router.put('/:id', (req,res) =>{
-    res.send(`Customer with id ${req.params.id} just updated!`);
+router.post("/", async (req, res) => {
+  try {
+    const customer = await Customer(req.body);
+    res.status(201).send({
+      success: true,
+      data: customer,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(error.message);
+  }
+  res.send(req.body);
+});
+
+router.put("/:id", (req, res) => {
+  res.send(`Customer with id ${req.params.id} just updated!`);
 });
 
 module.exports = router;
